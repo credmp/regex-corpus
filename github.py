b1051478f14868eb745897f0a2a4787d34b09c38
Arjen Wiersma <arjen@wiersma.org>
2023-10-08 18:39:09 +0200

    feat: fully working using python3 and the Github API

--- github.py
+++ github.py
@@ -1,11 +1,17 @@
-import requests
+#!/usr/bin/env python3
+import os
+import sys
 import re
-from codecs import open
-import getpass
 from os.path import exists
+import requests
+import time
 
-token = ""
+token = os.environ.get("GITHUB_TOKEN")
 output_file = "githubregexes.txt"
+
+if token is None:
+    print("No GITHUB_TOKEN")
+    sys.exit()
 
 re_re = re.compile("""(?:re\.)?[matchfindallitersearch]*\((?P<quote>['"])(?P<regex>.*?)(?P=quote)""")
 
@@ -16,33 +22,35 @@ regexes = set([])
 
 #load regexes from previous runs
 if exists(output_file):
-    with open(output_file,"r","utf8") as fid:
+    with open(output_file,"r",encoding="utf-8") as fid:
         for regex in fid:
             regexes.add(regex[:-1])
 
 print("Before:",len(regexes))
 
 for func in ["match","search","findall"]:
+    print("Finding {func} occurences")
     for i in range(1,11):
         params = dict(per_page=100,
                       page=i,
                       q=search_term.format(func))
         headers = dict(Accept="application/vnd.github.v3.text-match+json",
-                       Authorization="Bearer {}".format(token))
-        print(headers)
+                       Authorization=f"Bearer {token}")
         r = requests.get(codesearch_url,params=params,headers=headers)
 
         if r.ok:
-            for r in r.json()["items"]:
-                for m in r["text_matches"]:
+            print("processing page of results")
+            for ri in r.json()["items"]:
+                for m in ri["text_matches"]:
                     for match in re.finditer(re_re,m["fragment"]):
                         regexes.add(match.group("regex"))
         else:
             print("request failed: {}", r)
 
+        time.sleep(10)
+
 print("After",len(regexes))
 
-with open(output_file,"w","utf8") as fid:
+with open(output_file,"w", encoding="utf-8") as fid:
     for regex in regexes:
-        fid.write(u"{}\n".format(regex))
-
+        fid.write(f"{regex}\n")
--- regexlib.py
+++
@@ -1,6 +0,0 @@
-from zeep import Client
-
-client = Client("http://regexlib.com/WebServices.asmx?WSDL")
-print client.service.ListAllAsXml(maxrows=4)
-
-
