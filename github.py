c2946c5e1746934fa4b1756621da19b7d7d54ea9
Arjen Wiersma <arjen@wiersma.org>
2023-10-08 16:48:44 +0200

    feat: update to python3

--- github.py
+++ github.py
@@ -4,9 +4,7 @@ from codecs import open
 import getpass
 from os.path import exists
 
-username = raw_input("Username:")
-password = getpass.getpass("Password:")
-
+token = ""
 output_file = "githubregexes.txt"
 
 re_re = re.compile("""(?:re\.)?[matchfindallitersearch]*\((?P<quote>['"])(?P<regex>.*?)(?P=quote)""")
@@ -22,24 +20,27 @@ if exists(output_file):
         for regex in fid:
             regexes.add(regex[:-1])
 
-print "Before:",len(regexes)
+print("Before:",len(regexes))
 
 for func in ["match","search","findall"]:
     for i in range(1,11):
         params = dict(per_page=100,
                       page=i,
                       q=search_term.format(func))
-        auth=(username,password)
-        headers = dict(Accept="application/vnd.github.v3.text-match+json")
-        r = requests.get(codesearch_url,params=params,headers=headers,auth=auth)
+        headers = dict(Accept="application/vnd.github.v3.text-match+json",
+                       Authorization="Bearer {}".format(token))
+        print(headers)
+        r = requests.get(codesearch_url,params=params,headers=headers)
 
         if r.ok:
             for r in r.json()["items"]:
                 for m in r["text_matches"]:
                     for match in re.finditer(re_re,m["fragment"]):
                         regexes.add(match.group("regex"))
+        else:
+            print("request failed: {}", r)
 
-print "After",len(regexes)
+print("After",len(regexes))
 
 with open(output_file,"w","utf8") as fid:
     for regex in regexes:
